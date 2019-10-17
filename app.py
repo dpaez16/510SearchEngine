df26a169fc4218e642b97455e23b025c122438d9
Danny Paez <dpaez16@yahoo.com>
2019-10-17 18:15:04 -0500

    a bit of refactoring

--- app.py
+++ app.py
@@ -5,10 +5,21 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home_page():
-    query = None
     if request.method == "POST":
         query = request.form['query']
-    
+        if len(query) != 0:
+            return redirect(url_for('search', query=query), code=307) # POST request
+    return render_template('home.html')
+
+
+@app.route("/query=<string:query>", methods=["GET", "POST"])
+def search(query):
+    if request.method == "POST":
+        query = request.form["query"]
+        if len(query) == 0:
+            return redirect(url_for('home_page'))
+        return redirect(url_for('search', query=query)) # GET request
+    # process query here
     return render_template('home.html', query=query)
 
 
