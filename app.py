e7c9267bb63ff4d99a2fc8915e6dcd300d19f463
Danny Paez <dpaez16@yahoo.com>
2019-10-19 23:57:06 -0500

    phase 1

--- app.py
+++ app.py
@@ -1,23 +1,43 @@
 from flask import Flask, request, render_template, redirect, url_for, send_from_directory
+import logging
+from logging import handlers
 import os
 
 app = Flask(__name__)
 fileIDX = open('files.txt', 'r').readlines()
 
-def createLink(title):
-    return "https://www.aclweb.org/anthology/{}.pdf".format(title.split('.tei.xml')[0])
+# logging config
+logger = logging.getLogger('relevance_judgements')
+logger.setLevel(logging.DEBUG)
+handler = handlers.RotatingFileHandler('relevance-judgements.log', backupCount=5)
+logger.addHandler(handler)
 
-def getSearchResults():
+
+def logRelevance(query, idx, R):
+    logger.debug("({}, {}, {})".format(query, idx, R))
+    
+
+def createLink(fileName):
+    return "https://www.aclweb.org/anthology/{}.pdf".format(fileName.split('.tei.xml')[0])
+
+
+def getSearchResults(query):
     searchResultsFile = open('searchResults.txt', 'r')
-    IDXs = searchResultsFile.readlines()
-    IDXs = [int(idx.strip()) for idx in IDXs]
-    searchResults = [fileIDX[idx].strip() for idx in IDXs]
-    searchResults = [createLink(fileName) for fileName in searchResults]
+    rawSearchResults = searchResultsFile.readlines()
+    searchResults = []
+    for rawSearchResult in rawSearchResults:
+        idx, content = rawSearchResult.strip().split('\t')
+        idx = int(idx)
+        splitted = fileIDX[idx].strip().split('\t') + [""]
+        fileName, title = splitted[:2]
+        link = createLink(fileName)
+        title = link if len(title) == 0 else title
+        searchResults.append((title, content, idx))
+        logRelevance(query, idx, 0)
     searchResultsFile.close()
+    os.remove('searchResults.txt')
     return searchResults
 
-def logClick(query, link, R):
-    print("({}, {}, {})".format(query, link, R))
 
 @app.route("/", methods=["GET", "POST"])
 def home_page():
@@ -37,11 +57,19 @@ def search(query):
         return redirect(url_for('search', query=query)) # GET request
     # process query here
     os.system('python3 search.py \"{}\" {} -f'.format(query, str(10)))
-    searchResults = getSearchResults()
+    searchResults = getSearchResults(query)
     return render_template('home.html', 
                             searchResults=searchResults, 
                             query=query,
-                            logClick=logClick)
+                            logRelevance=logRelevance)
+
+
+@app.route("/query=<string:query>,paper=<int:paperIdx>")
+def goToPaper(query, paperIdx):
+    logRelevance(query, paperIdx, 1)
+    fileName = fileIDX[paperIdx].strip().split('\t')[0]
+    link = createLink(fileName)
+    return redirect(link)
 
 
 if __name__ == "__main__":
